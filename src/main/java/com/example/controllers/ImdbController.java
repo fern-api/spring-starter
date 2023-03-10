6c13dec88463d243be2c2e51ba5c9a43db77f4a2
dsinghvi <deep@usebirch.com>
2023-03-10 01:02:29 -0800

    save basic java spring repo

---
+++ src/main/java/com/example/controllers/ImdbController.java
@@ -0,0 +1,25 @@
+package com.example.controllers;
+
+import com.example.generated.resources.imdb.ImdbService;
+import com.example.generated.resources.imdb.types.CreateMovieRequest;
+import com.example.generated.resources.imdb.types.Movie;
+import com.example.generated.resources.imdb.types.MovieId;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public final class ImdbController implements ImdbService {
+
+    @Override
+    public MovieId createMovie(CreateMovieRequest body) {
+        return null;
+    }
+
+    @Override
+    public Movie getMovie(MovieId movieId) {
+        return Movie.builder()
+                .id(movieId)
+                .title("Goodwill hunting")
+                .rating(10.9)
+                .build();
+    }
+}
